cbb88f1503bb1b20a4a9914d07d64aced14c062b
Tommy Su <tommy.t.su@gmail.com>
2023-03-15 19:58:54 -0700

    add initial iOS project

---
+++ gophergo/gophergo/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  gophergo
+//
+//  Created by Tommy Su on 3/12/23.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadPie()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ gophergo/gophergoTests/gophergoTests.swift
@@ -0,0 +1,33 @@
+//
+//  gophergoTests.swift
+//  gophergoTests
+//
+//  Created by Tommy Su on 3/12/23.
+//
+
+import XCTest
+@testable import gophergo
+
+class gophergoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
